08ef35a4f8b81fe0ea5bd683821e96a3d33ad3ce
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-09-23 15:33:12 +0200

    add db model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
@@ -11,13 +12,35 @@ const handler = NextAuth({
     ],
 
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({ profile }) {
         try {
+            await connectToDB();
 
+            // check if the user exist
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+            // if not, create user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
+            return true;
         } catch (error) {
-            
+            console.log(error);
+            return false;
         }
     }
 })
